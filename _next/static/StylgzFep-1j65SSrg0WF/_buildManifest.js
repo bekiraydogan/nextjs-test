e4525363cf97078f7c7d1108ce947eda99b44d60
bekiraydogan <byas66@gmail.com>
2022-08-03 20:45:01 +0000

    Deploying to gh-pages from @ bekiraydogan/nextjs-test@f296d3db56651c2c3fa6179c6c4c1a6b425b0c31 🚀

---
+++ _next/static/StylgzFep-1j65SSrg0WF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-dcb2c8d1d7a6d1f2.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
